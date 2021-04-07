fd9d9d5be20a20eefcfb05fb500a640ef3086513
Greg <gregkaplo@outlook.com>
2021-04-07 12:10:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,28 @@
-var item1;
-var item2;
-var item3;
-document.getElementById("choose").onclick = make_selection;
+document.getElementById("generate").onclick = roll;
 
-function make_selection() {
-  item1 = prompt("enter new 1")
-  item2 = prompt("enter new 2")
-  item3 = prompt("enter new 3")
-  updaitList();
+
+function roll() {
+  var seed = Math.floor((Math.random() * 8));
+  // document.getElementById("card").src = list[seed];
+	var result_img = list[seed]
+
+  //Get our img element by using document.getElementById
+  var img123 = document.getElementById("card");
+
+  //Set the src property of our element to the new image URL
+  img123.src = result_img;
+
+
+
 }
+var list = [
+  "https://c1.scryfall.com/file/scryfall-cards/normal/front/9/4/942cf220-472c-48f6-8f60-993939ea5ab8.jpg?1562055436",
+  "https://c1.scryfall.com/file/scryfall-cards/normal/front/e/3/e319956e-982b-4bba-829a-67bf4ef79d74.jpg?1576382565",
+  "https://c1.scryfall.com/file/scryfall-cards/normal/front/9/0/9082bfb8-0ab0-4378-976d-a2c9d3c35a5e.jpg?1562921827",
+  "https://c1.scryfall.com/file/scryfall-cards/normal/front/7/9/790157c9-b1ed-4da5-9d50-e99e0dd807b7.jpg?1562920405",
+  "https://c1.scryfall.com/file/scryfall-cards/normal/front/1/3/13281377-6d16-4ed3-8aa6-c47d6eb783df.jpg?1562899536",
+"https://c1.scryfall.com/file/scryfall-cards/normal/front/3/2/32987720-cc0c-416b-b79b-217d3b37542d.jpg?1562629169",
+"https://c1.scryfall.com/file/scryfall-cards/normal/front/9/9/993957c7-5d61-4c98-826d-b657aed667e1.jpg?1562878704",
+"https://c1.scryfall.com/file/scryfall-cards/normal/front/7/8/78fec7e6-5ed9-46dc-93b4-7a054d763403.jpg?1600714793"
 
-function updaitList() {
-  document.getElementById("first").innerHTML = item1;
-  document.getElementById("second").innerHTML = item2;
-  document.getElementById("third").innerHTML = item3;
-}
-
-var light_mode = "black";
-document.getElementById("brightnes").onclick = bright_mode;
-
-function bright_mode() {
-  if (light_mode == "white") {
-    light_mode = "black";
-  } else if (light_mode == "black") {
-    light_mode = "white";
-  }
-  updait_colormode();
-}
-
-function updait_colormode() {
-  if (light_mode == "black") {
-    var all = document.getElementsByTagName("*");
-
-    for (var i = 0, max = all.length; i < max; i++) {
-      all[i].style.color = "black";
-      document.body.style.backgroundColor = "white";
-    }
-  }
-  if (light_mode == "white") {
-    var all = document.getElementsByTagName("*");
-
-    for (var i = 0, max = all.length; i < max; i++) {
-      all[i].style.color = "white";
-      document.body.style.backgroundColor = "black";
-    }
-  }
-}
+];
